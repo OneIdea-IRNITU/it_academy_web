ca8068e99f18f5fbfcd4f4ea998e1db776d6dc77
Алексей <alexleskov2000@gmail.com>
2021-05-02 18:14:35 +0800

    Добавить основные библиотеки

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuelidate from 'vuelidate'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(Vuelidate)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
