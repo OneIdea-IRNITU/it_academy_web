bae1592eba3631f0d454fa0c980814b3e57e23ef
Алексей <alexleskov2000@gmail.com>
2021-05-15 23:24:35 +0800

    Убрать вывод пустого времени

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -18,6 +18,12 @@
             <a class="category">#{{ event.category }}</a>
 
             <p class="card-text">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                   class="bi bi-calendar-event" viewBox="0 0 16 16">
+                <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+                <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+              </svg>
               <span class="startdate"> {{ event.startdate }}</span>
               <span v-if="event.enddate>0" class="enddate"> - {{ event.enddate }}</span>
             </p>
@@ -70,12 +76,12 @@ export default {
 
                 if (event.startdate > 0) {
                   let startdate = new Date(event.startdate * 1000)
-                  event.startdate = startdate.toLocaleString().replace(',', '').slice(0, -3)
+                  event.startdate = startdate.toLocaleString().replace(',', '').slice(0, -3).replace('00:00', '')
 
                 }
                 if (event.enddate > 0) {
                   let enddate = new Date(event.enddate * 1000)
-                  event.enddate = enddate.toLocaleString().replace(',', '').slice(0, -3)
+                  event.enddate = enddate.toLocaleString().replace(',', '').slice(0, -3).replace('00:00', '')
                 }
 
                 events.push(event)
