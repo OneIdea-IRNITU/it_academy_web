b18531993f0fee17fc2551f50b60ef90bc028362
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2021-05-30 20:43:51 +0800

    Сделать что-то

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -9,14 +9,34 @@
       </span>
       Minutes: {{ minutes | formatTime }} <br/>
       Seconds: {{ seconds | formatTime }} <br/>
+
+      <span v-if="FinalCompare">
         <br/>
-        Осталось: <span v-if="days">{{ days }}</span> дней<br />
+        Осталось: <span v-if="FinalComparefinc">{{ days }}</span> дней<br />
+      </span>
      </div>
     <!-- <div class="text-center" v-if="!currentTime">
       Уже сегодня!
     </div> -->
-    <div class="text-center" v-if= lastCompare>
-        Уже сегодня!
+
+      <!-- 
+      deadlineDay
+      deadlineMonth
+      deadlineYear
+      nowDay
+      nowMonth
+      nowYear
+      isExactYear
+      isExactMonth
+      isExactDay
+      FirstCompare
+      FinalCompare -->
+
+
+    <div class="text-center" v-if="FinalComparefinc" :load="log(FinalComparefinc)">
+        Уже сегодня! <br/>
+        <span v-if="days">{{ days }}:</span
+        ><span v-if="days">{{ hours | formatTime }}:</span><span>{{ minutes | formatTime }}:{{ seconds | formatTime }}</span><br />
     </div>
   </div>
 </template>
@@ -27,7 +47,7 @@ export default {
     deadline: {
       type: String,
     //   В дефолт вписываем значение "Y-M-D", либо "Y-М-DTH:M:S"
-      default: "2021-05-31T15:24:00",
+      default: "2021-06-30",
     //   Раскомментить, после оформления подсоса времени с базы
     //   required: true,
     },
@@ -41,18 +61,15 @@ export default {
     // Куррент тайм - оставшееся время
       currentTime: Date.parse(this.deadline) - Date.parse(new Date()),
     // "Сегодняшнее число/месяц" и "Число/месяц мероприятия"
-      deadlineDay: (this.deadline).getDate,
-      deadlineMonth: (this.deadline).getMonth,
-      deadlineYear: (this.deadline).getFullYear,
-      nowDay: (new Date).getDate(),
-      nowMonth: (new Date).getMonth(),
-      nowYear: (new Date).getFullYear(),
-    // Сравнение предыдущих переменных для вывода "Мероприятие Сегодня"
-      isExactYear: this.deadlineYear === this.nowYear,
-      isExactMonth: this.deadlineMonth === this.nowMonth,
-      isExactDay: this.deadlineDay === this.nowDay,
-    // Всё совпало, всё TRUE
-      lastCompare: this.isExactYear === this.isExactMonth === this.isExactDay,
+    //   datacompare: new Date(this.deadline) === new Date(),
+      deadlineDay: (new Date(this.deadline)).getDate(),
+      deadlineMonth: (new Date(this.deadline)).getMonth()+1,
+      deadlineYear: (new Date(this.deadline)).getFullYear(),
+      nowDay: (new Date()).getDate(),
+      nowMonth: (new Date()).getMonth()+1,
+      nowYear: (new Date()).getFullYear(),
+
+      isExactYear: Boolean,
     };
   },
   mounted() {
@@ -72,6 +89,30 @@ export default {
       return Math.floor(this.currentTime / (1000 * 60 * 60 * 24));
     }
   },
+  watch:{
+    isExactYearfunc(){
+       return this.deadlineYear === this.nowYear
+    },
+    isExactMonthfunc(){
+       return this.deadlineMonth === this.nowMonth
+    },
+    isExactDayfunc(){
+       return this.deadlineDay === this.nowDay
+    },
+    // FirstCompare(){
+    //    return this.isExactYear === this.isExactMonth
+    // },
+    FinalComparefinc(){
+      this.isExactYear = (this.deadlineYear === this.nowYear)
+    //   this.isExactMonth = this.deadlineMonth === this.nowMonth
+    //   this.isExactDay = this.deadlineDay === this.nowDay
+    
+    //   this.FirstCompare = this.isExactYear === this.isExactMonth
+    //   this.FinalCompare = this.FirstCompare === this.isExactDay
+      return this.FinalCompare
+    }
+  },
+
   filters: {
     formatTime(value) {
       if (value < 10) {
@@ -88,9 +129,15 @@ export default {
       } else {
         this.currentTime = null;
       }
+    },
+    log(item) {
+      console.log(item)
+    },
+    compare(a, b) {
+        return a === b
     }
   }
 }
-console.log(new Date("2021-05-31T15:24:00").getFullYear() === (new Date).getFullYear())
+
 </script>
 
